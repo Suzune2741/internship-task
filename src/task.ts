952c65f5c2be6f0f2a8b82d8ab17c442444b3012
Suzune2741 <kannra1174@gmail.com>
2025-06-12 15:59:56 +0900

    chore: コメントを追加

--- src/task.ts
+++ src/task.ts
@@ -9,10 +9,13 @@ const isValid = (s: string) => {
   const stack: string[] = [];
   for (const c of s) {
     if (front[c]) {
+      //開き括弧ならstackに積む
       stack.push(c);
     }else
     {
+      //そうでなければstackからpop
       const before = stack.pop();
+      //関係づけられている括弧と一致しているか確認
       if(front[before!] !== c)
         return false;
     }
